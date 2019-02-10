4087ccf59ab887869622608994b810c010c081c3
yijigao <yijigao92@gmail.com>
2019-02-10 16:59:31 +0800

    login and register

--- app/models.py
+++ app/models.py
@@ -1,8 +1,9 @@
-from app import db
+from app import db, login
 from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
@@ -26,3 +27,7 @@ class Post(db.Model):
 
     def __repr__(self):
         return '<Post {self.body}>'
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
