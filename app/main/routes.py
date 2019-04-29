b136ea92a80ee0e99c5a56576a46b2ff90c14393
yijigao <yijigao92@gmail.com>
2019-04-29 21:18:48 +0800

    fix profie..

--- app/main/routes.py
+++ app/main/routes.py
@@ -60,8 +60,8 @@ def user(username):
     user = User.query.filter_by(username=username).first_or_404()
     page = request.args.get('page', 1, type=int)
     posts = user.posts.order_by(Post.timestamp.desc()).paginate(page, current_app.config["POSTS_PER_PAGE"], False)
-    next_url = url_for('main.user', page=posts.next_num) if posts.has_next else None
-    prev_url = url_for('main.user', page=posts.prev_num) if posts.has_prev else None
+    next_url = url_for('main.user', username=user.username, page=posts.next_num) if posts.has_next else None
+    prev_url = url_for('main.user', username=user.username, page=posts.prev_num) if posts.has_prev else None
     return render_template('user.html', user=user, posts=posts.items, next_url=next_url, prev_url=prev_url)
 
 @bp.route('/edit_profile', methods=['GET', 'POST'])
