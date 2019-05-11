1c9e929eeff5a3c6555a4ff0f0373757952e0bf4
yijigao <yijigao92@gmail.com>
2019-05-11 20:06:17 +0800

    bootstrap pagination

--- app/main/routes.py
+++ app/main/routes.py
@@ -35,11 +35,12 @@ def index():
         flash('动态发布成功！')
         return redirect(url_for('main.index'))
     page = request.args.get('page', 1, type=int)
-    posts = current_user.followed_posts().paginate(
+    pagination = current_user.followed_posts().paginate(
         page, current_app.config['POSTS_PER_PAGE'], False)
-    next_url = url_for('main.index', page=posts.next_num) if posts.has_next else None
-    prev_url = url_for('main.index', page=posts.prev_num) if posts.has_prev else None
-    return render_template('index.html', title='Home', form=form, posts=posts.items, next_url=next_url, prev_url=prev_url)
+    posts = pagination.items
+    # next_url = url_for('main.index', page=posts.next_num) if posts.has_next else None
+    # prev_url = url_for('main.index', page=posts.prev_num) if posts.has_prev else None
+    return render_template('index.html', title='Home', form=form, posts=posts, pagination=pagination)
 
 @bp.route('/explore')
 @login_required
@@ -61,10 +62,11 @@ def explore():
 def user(username):
     user = User.query.filter_by(username=username).first_or_404()
     page = request.args.get('page', 1, type=int)
-    posts = user.posts.order_by(Post.timestamp.desc()).paginate(page, current_app.config["POSTS_PER_PAGE"], False)
-    next_url = url_for('main.user', username=user.username, page=posts.next_num) if posts.has_next else None
-    prev_url = url_for('main.user', username=user.username, page=posts.prev_num) if posts.has_prev else None
-    return render_template('user.html', user=user, posts=posts.items, next_url=next_url, prev_url=prev_url)
+    pagination = user.posts.order_by(Post.timestamp.desc()).paginate(page, current_app.config["POSTS_PER_PAGE"], False)
+    # next_url = url_for('main.user', username=user.username, page=posts.next_num) if posts.has_next else None
+    # prev_url = url_for('main.user', username=user.username, page=posts.prev_num) if posts.has_prev else None
+    posts = pagination.items
+    return render_template('user.html', user=user, posts=posts, pagination=pagination)
 
 @bp.route('/edit_profile', methods=['GET', 'POST'])
 @login_required
@@ -116,16 +118,16 @@ def unfollow(username):
 
 
 
-@bp.route('/search')
-@login_required
-def search():
-    if not g.search_form.validate():
-        return redirect(url_for("main.explore"))
-    page = request.args.get('page', 1, type=int)
-    posts, total = Post.search(g.search_form.q.data, page, current_app.config['POSTS_PER_PAGE'])
-    next_url = url_for('main.search', q=g.search_form.q.data, page=page+1) if total > page*current_app.config['POSTS_PER_PAGE'] else None
-    prev_url = url_for('main.search', q=g.search_form.q.data, page=page+1) if page > 1 else None
-    return render_template("search.html", title='Search', posts=posts, next_url=next_url, prev_url=prev_url)
+# @bp.route('/search')
+# @login_required
+# def search():
+#     if not g.search_form.validate():
+#         return redirect(url_for("main.explore"))
+#     page = request.args.get('page', 1, type=int)
+#     posts, total = Post.search(g.search_form.q.data, page, current_app.config['POSTS_PER_PAGE'])
+#     next_url = url_for('main.search', q=g.search_form.q.data, page=page+1) if total > page*current_app.config['POSTS_PER_PAGE'] else None
+#     prev_url = url_for('main.search', q=g.search_form.q.data, page=page+1) if page > 1 else None
+#     return render_template("search.html", title='Search', posts=posts, next_url=next_url, prev_url=prev_url)
 
 
 @bp.route('/send_message/<recipient>', methods=['GET', 'POST'])
@@ -149,10 +151,11 @@ def messages():
     current_user.add_notification('未读私信', 0)
     db.session.commit()
     page = request.args.get('page', 1, type=int)
-    messages = current_user.messages_received.order_by(Message.timestamp.desc()).paginate(page, current_app.config['POSTS_PER_PAGE'], False)
-    next_url = url_for('main.messages', page=messages.next_num) if messages.has_next else None
-    prev_url = url_for("main.messages", page=messages.prev_num) if messages.has_prev else None
-    return render_template('messages.html', messages=messages.items, next_url=next_url, prev_url=prev_url)
+    pagination = current_user.messages_received.order_by(Message.timestamp.desc()).paginate(page, current_app.config['POSTS_PER_PAGE'], False)
+    # next_url = url_for('main.messages', page=messages.next_num) if messages.has_next else None
+    # prev_url = url_for("main.messages", page=messages.prev_num) if messages.has_prev else None
+    messages = pagination.items
+    return render_template('messages.html', messages=messages, pagination=pagination)
 
 @bp.route('/notifications')
 @login_required
